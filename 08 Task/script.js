a750d1305c3f529c3b3cba52ec4f81347a80648c
500appsmounika <mounika.500apps@gmail.com>
2023-01-27 13:11:45 +0530

    intermediate scripting algorithm
    Signed-off-by: 500appsmounika <mounika.500apps@gmail.com>

---
+++ 08 Task/script.js
@@ -0,0 +1,12 @@
+function pairElement(str) {
+    let obj={
+      "A":"T",
+       "T":"A",
+       "C":"G",
+       "G":"C"  
+    }
+    return str.split("").map(item=>[item,obj[item]]);
+  }
+  function getOutput(){
+  document.getElementById("output").textContent=pairElement("GCG");
+  }
